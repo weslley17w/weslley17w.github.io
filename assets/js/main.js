685e2ea41773f0f80fd0399f8ace04614ae18e92
Weslley Barros <weslley17w@gmail.com>
2016-07-30 22:11:55 -0300

    Site updated at 2016-07-30 22:11:52

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,13 +6,7 @@
 
 (function($) {
 
-	skel.breakpoints({
-		xlarge:	'(max-width: 1680px)',
-		large:	'(max-width: 1280px)',
-		medium:	'(max-width: 980px)',
-		small:	'(max-width: 736px)',
-		xsmall:	'(max-width: 480px)'
-	});
+
 
 	$(function() {
 
@@ -95,7 +89,12 @@
 		$form.placeholder();
 
 		// Prioritize "important" elements on medium.
-
+		skel.on('+medium -medium', function() {
+			$.prioritize(
+				'.important\\28 medium\\29',
+				skel.breakpoint('medium').active
+			);
+		});
 
 		// Menu.
 		var $menu = $('#menu');
